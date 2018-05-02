7fb6faf6d5f05a772b00931debb045dfa3e6c8c1
sophie_aerdker <sophie.aerdker@protonmail.com>
2018-05-02 09:53:40 +0200

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+
+x = 6.0
+y = 6.0
+
+print "x = %f and y = %f" %(x,y)
+if (x > y):
+    print "x is greater than y!"
+elif(x < y): print "x is smaller than y!"
+else:
+    print "x is equal y!"
+    if x and y >= 5.0:
+        print "x and y are greater than 5!"
+
+print "Finish..."
+
+char = "h"
+string = "Hello World"
+
+if char in string:
+    print "yes"
+else: print "no"
